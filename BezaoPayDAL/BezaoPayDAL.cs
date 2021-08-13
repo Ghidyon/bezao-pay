7b34c950e39e8cebdab9082761c6bbc422f8f3fb
Ghidyon <gideonakunana@gmail.com>
2021-08-13 08:10:40 +0300

    initial commit

---
+++ BezaoPayDAL/BezaoPayDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BezaoPayDAL
+{
+    public class BezaoPayDAL
+    {
+        private readonly string _connectionString;
+
+        private SqlConnection _sqlConnection = null;
+
+        // Default constructor with chaining (Constructor Overloading)
+        public BezaoPayDAL() : 
+            this(@"Data Source = (localdb)\mssqllocaldb;Integrated Security=true;Initial Catalog=BezaoPay")
+        { 
+        }
+
+        public BezaoPayDAL(string connectionString)
+            => _connectionString = connectionString;
+
+        private void OpenConnection()
+        {
+
+        }
+    }
+}
