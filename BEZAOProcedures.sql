25eed6d2a0af8db10358ff0cd3dd60df338dbb0c
Ghidyon <gideonakunana@gmail.com>
2021-08-14 22:52:22 +0100

    added procedures

---
+++ BEZAOProcedures.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE CreateUser
+@name char(50),
+@email char(50),
+@userId int,
+@accountNumber int,
+@balance decimal(38,2) output
+AS
+INSERT INTO Users (Name, Email) VALUES (@name, @email);
+
+Go
+
+CREATE PROCEDURE GetId
+@name char(50),
+@id int output
+AS
+SELECT @id = Id from Users where Name = @name;
+
+Go
+
+CREATE PROCEDURE CreateAccount
+@userId int,
+@accountNumber int,
+@balance decimal(38, 2) output
+AS
+INSERT INTO Accounts (UserId, Account_Number, Balance) VALUES (@userId, @accountNumber, @decimal);
